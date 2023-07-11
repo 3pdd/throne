be4d4428d2b3ee727f217b33fe9d5d7c38d931d8
Jerry Renn <jerryrenn1998@gmail.com>
2023-07-11 13:43:00 -0700

    fast food data

---
+++ fastFoodData.js
@@ -0,0 +1,488 @@
+module.exports = {
+  data: [
+    {
+      "name": "Famiglia Pizza",
+      "address": "757 Broadway (At 8th Street)"
+    },
+    {
+      "name": "McDonald's",
+      "address": "34th Street and 10th Avenue, NY, NY"
+    },
+    {
+      "name": "Indikitch",
+      "address": "25 West 23 Street"
+    },
+    {
+      "name": "Burger King",
+      "address": "4275 White Plains Rd"
+    },
+    {
+      "name": "Burger King",
+      "address": "3901 Richmond Ave"
+    },
+    {
+      "name": "Burger King",
+      "address": "3500 Boston Rd"
+    },
+    {
+      "name": "Burger King",
+      "address": "6007 Broadway"
+    },
+    {
+      "name": "Burger King",
+      "address": "1590 Hutchinson River Pkwy"
+    },
+    {
+      "name": "Burger King",
+      "address": "2179 White Plains Rd"
+    },
+    {
+      "name": "Burger King",
+      "address": "2655 Ricmond Ave"
+    },
+    {
+      "name": "Burger King",
+      "address": "2557 Richmond Ave"
+    },
+    {
+      "name": "Burger King",
+      "address": "215-213 E Forham Rd"
+    },
+    {
+      "name": "Burger King",
+      "address": "2800 Hylan Blvd"
+    },
+    {
+      "name": "Burger King",
+      "address": "2036-2048 Jerome Ave"
+    },
+    {
+      "name": "Burger King",
+      "address": "1851-1859 Bruckner Blvd"
+    },
+    {
+      "name": "Burger King",
+      "address": "400 East Tremont Ave"
+    },
+    {
+      "name": "Burger King",
+      "address": "1419 Saint Nicholas Ave"
+    },
+    {
+      "name": "Burger King",
+      "address": "1380 Jerome Ave"
+    },
+    {
+      "name": "Burger King",
+      "address": "120-102 Liberty Ave"
+    },
+    {
+      "name": "Burger King",
+      "address": "977 South Blvd"
+    },
+    {
+      "name": "Burger King",
+      "address": "950 Jewett Ave"
+    },
+    {
+      "name": "Burger King",
+      "address": "1565 Hylan Blvd"
+    },
+    {
+      "name": "Burger King",
+      "address": "20 Willowbrook Rd"
+    },
+    {
+      "name": "Burger King",
+      "address": "103-107 E 161 St"
+    },
+    {
+      "name": "Burger King",
+      "address": "521 E 149th St"
+    },
+    {
+      "name": "Burger King",
+      "address": "557 Grand Concourse"
+    },
+    {
+      "name": "Burger King",
+      "address": "2425 Coney Island Ave"
+    },
+    {
+      "name": "Burger King",
+      "address": "23-43 Utica Ave"
+    },
+    {
+      "name": "Burger King",
+      "address": "1772 Rockaway Pkwy"
+    },
+    {
+      "name": "Burger King",
+      "address": "624 S Conduit Blvd"
+    },
+    {
+      "name": "Burger King",
+      "address": "39-24 104th St"
+    },
+    {
+      "name": "Burger King",
+      "address": "2164 86th St"
+    },
+    {
+      "name": "Burger King",
+      "address": "130 W 125th St"
+    },
+    {
+      "name": "Burger King",
+      "address": "1940 Linden Blvd"
+    },
+    {
+      "name": "Burger King",
+      "address": "154 E 116 St"
+    },
+    {
+      "name": "Burger King",
+      "address": "8002 Foster Ave"
+    },
+    {
+      "name": "Burger King",
+      "address": "13 Hillel Pl"
+    },
+    {
+      "name": "Burger King",
+      "address": "33-10 Asotria Blvd"
+    },
+    {
+      "name": "Burger King",
+      "address": "1886 3rd Ave"
+    },
+    {
+      "name": "Burger King",
+      "address": "5212-15 5th Ave"
+    },
+    {
+      "name": "Burger King",
+      "address": "1412 Myrtle Ave"
+    },
+    {
+      "name": "Burger King",
+      "address": "3301 Ft Hamilton Pkwy"
+    },
+    {
+      "name": "Burger King",
+      "address": "9006 Church Ave"
+    },
+    {
+      "name": "Burger King",
+      "address": "1727 Bedford Ave"
+    },
+    {
+      "name": "Burger King",
+      "address": "4101 4th Ave"
+    },
+    {
+      "name": "Burger King",
+      "address": "957 Marcy Ave"
+    },
+    {
+      "name": "Burger King",
+      "address": "736 Broadway"
+    },
+    {
+      "name": "Burger King",
+      "address": "6413 14th Ave"
+    },
+    {
+      "name": "Burger King",
+      "address": "226 E 86th St"
+    },
+    {
+      "name": "Burger King",
+      "address": "327 W 42nd St"
+    },
+    {
+      "name": "Burger King",
+      "address": "971 Flatbush Ave"
+    },
+    {
+      "name": "Burger King",
+      "address": "401-403 5th Ave"
+    },
+    {
+      "name": "Burger King",
+      "address": "474 7th Ave"
+    },
+    {
+      "name": "Burger King",
+      "address": "1313 Broadway"
+    },
+    {
+      "name": "Burger King",
+      "address": "3310 21st St"
+    },
+    {
+      "name": "Burger King",
+      "address": "417-21 Fulton St"
+    },
+    {
+      "name": "Burger King",
+      "address": "146 Delancey St"
+    },
+    {
+      "name": "Burger King",
+      "address": "325 Broadway"
+    },
+    {
+      "name": "Burger King",
+      "address": "106 Liberty St"
+    },
+    {
+      "name": "Burger King",
+      "address": "106 Fulton St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "824 3rd Ave 50th St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "1188 6th Ave"
+    },
+    {
+      "name": "McDonald's",
+      "address": "735 9th Ave"
+    },
+    {
+      "name": "McDonald's",
+      "address": "969 1st Ave"
+    },
+    {
+      "name": "McDonald's",
+      "address": "1651 Broadway"
+    },
+    {
+      "name": "McDonald's",
+      "address": "3410 Broadway 138th St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "139th & Adam Clayton Powell"
+    },
+    {
+      "name": "McDonald's",
+      "address": "85 Bruckner Blvd"
+    },
+    {
+      "name": "McDonald's",
+      "address": "946 8th Ave & 56th"
+    },
+    {
+      "name": "McDonald's",
+      "address": "966 3rd Ave"
+    },
+    {
+      "name": "McDonald's",
+      "address": "444 Lenox Ave/132nd"
+    },
+    {
+      "name": "McDonald's",
+      "address": "600 W 125th at Broadway"
+    },
+    {
+      "name": "McDonald's",
+      "address": "1286 1st Ave at 69th St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "125th St & Lexington Ave"
+    },
+    {
+      "name": "McDonald's",
+      "address": "2049 Boradway at 71st"
+    },
+    {
+      "name": "McDonald's",
+      "address": "354 W 125th St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "148 W 125th St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "2142 3rd Ave"
+    },
+    {
+      "name": "McDonald's",
+      "address": "2271 Broadway"
+    },
+    {
+      "name": "McDonald's",
+      "address": "1997 3rd Ave"
+    },
+    {
+      "name": "McDonald's",
+      "address": "1499 3rd Ave & 85th St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "2726 Broadway"
+    },
+    {
+      "name": "McDonald's",
+      "address": "1871 2nd Ave"
+    },
+    {
+      "name": "McDonald's",
+      "address": "2549 Broadway & 96th St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "1872 3rd Ave at 103 Rd St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "14 E 47th St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "556 7th Ave"
+    },
+    {
+      "name": "McDonald's",
+      "address": "490 8th Ave"
+    },
+    {
+      "name": "McDonald's",
+      "address": "335 8th Ave"
+    },
+    {
+      "name": "McDonald's",
+      "address": "18 E 42nd St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "429 7th Ave"
+    },
+    {
+      "name": "McDonald's",
+      "address": "151 West 34th St & 7th Ave"
+    },
+    {
+      "name": "McDonald's",
+      "address": "280 Madison Ave"
+    },
+    {
+      "name": "McDonald's",
+      "address": "972 6th Ave"
+    },
+    {
+      "name": "McDonald's",
+      "address": "729 2nd Ave/29th St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "809/811 6th Ave/28th"
+    },
+    {
+      "name": "McDonald's",
+      "address": "341 5th Ave"
+    },
+    {
+      "name": "McDonald's",
+      "address": "480 3rd Ave & 33rd St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "401 Park Ave S"
+    },
+    {
+      "name": "McDonald's",
+      "address": "336 E 23rd St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "26 E 23rd St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "686 6th Ave"
+    },
+    {
+      "name": "McDonald's",
+      "address": "395 Flatbush Ave"
+    },
+    {
+      "name": "McDonald's",
+      "address": "325 Grove St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "39 Union Sq W"
+    },
+    {
+      "name": "McDonald's",
+      "address": "420 Fulton St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "541 6th Ave & 14th St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "404 E 14th St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "82 Court St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "102-106 1st Ave at 5th Ave"
+    },
+    {
+      "name": "McDonald's",
+      "address": "724 Broadway"
+    },
+    {
+      "name": "McDonald's",
+      "address": "136 W 3rd St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "208 Varrick St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "114 Delancey St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "213 Madison St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "26 Bowery"
+    },
+    {
+      "name": "McDonald's",
+      "address": "262 Canal St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "167 Chambers St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "160 Broadway"
+    },
+    {
+      "name": "McDonald's",
+      "address": "52 Fulton St/Cliff St"
+    },
+    {
+      "name": "McDonald's",
+      "address": "317 Broadway"
+    }
+  ]
+}
